c295f95ca61eb6ca11e286bfa85a268880eed54d
ravikiranbvn <ravi.has.kiran@gmail.com>
2024-03-02 15:07:18 +0100

    added x-tools for rpi4

---
+++ x-tools/aarch64-rpi4-linux-gnu/lib/gcc/aarch64-rpi4-linux-gnu/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/ravi/crosstool-ng/.build/aarch64-rpi4-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-rpi4-linux-gnu --prefix=/home/ravi/x-tools/aarch64-rpi4-linux-gnu --exec_prefix=/home/ravi/x-tools/aarch64-rpi4-linux-gnu --with-sysroot=/home/ravi/x-tools/aarch64-rpi4-linux-gnu/aarch64-rpi4-linux-gnu/sysroot --enable-languages=c,c++ --with-cpu=cortex-a72 --with-pkgversion='crosstool-NG 1.26.0' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/home/ravi/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-mpfr=/home/ravi/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-mpc=/home/ravi/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-isl=/home/ravi/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/home/ravi/x-tools/aarch64-rpi4-linux-gnu/aarch64-rpi4-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a72" } };
